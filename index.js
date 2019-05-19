fefb268084958a2d349e617d762efc6cabb35bf3
Anil Kishan <kishan.anil@gmail.com>
2019-05-19 00:36:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ function happyHolidaysTo(name) {
   console.log(`Happy holidays, ${name}!`)
 }
  
-function happyHolidaysTo(name) {
-  console.log(`Happy holidays, ${name}!`)
+function happyHolidaysTo(holiday, name) {
+  console.log(`Happy ${holiday}, ${name}!`)
 }
- 
+
+function holidayCountdow(days, holiday) {
+  console.log(`Happy ${holiday}, ${name}!`)
+}
