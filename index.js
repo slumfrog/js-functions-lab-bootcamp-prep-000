5634853d6bf4d2d57de68448a420c00d26cd223c
Anil Kishan <kishan.anil@gmail.com>
2019-05-19 00:37:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ function happyHolidaysTo(name) {
   console.log(`Happy holidays, ${name}!`)
 }
  
-function happyHolidaysTo(name) {
-  console.log(`Happy holidays, ${name}!`)
-}
- 
 function happyHolidaysTo(holiday, name) {
   console.log(`Happy ${holiday}, ${name}!`)
 }
