ac51483ea13d06882127e6e96c5f8f590ac63597
Anil Kishan <kishan.anil@gmail.com>
2019-05-19 00:43:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holiday, days){
-  return `It's ${days} days until ${holiday}`
+function holidayCountdown(days, holiday){
+  return `It's ${days} days until ${holiday}!`
 }
